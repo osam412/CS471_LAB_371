21dcbc6e8f1e28a4150631da62ae197230065f22
osama <osama.f22122@gmail.com>
2024-10-10 05:13:14 +0300

    Lab 4

--- DjanoCS471/urls.py
+++ DjanoCS471/urls.py
@@ -20,6 +20,6 @@ from django.urls import include, path
 urlpatterns = [
  path('admin/', admin.site.urls),
  path('books/', include("apps.bookmodule.urls")), 
- path('users/', include("apps.usermodule.urls")) 
+ path('users/', include("apps.usermodule.urls")) ,
 ]
 
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
 
+
 def index(request):
-    name = request.GET.get("name") or "world!"
-    return render(request, "bookmodule/index.html" , {"name": name})   
+   return render(request, "bookmodule/index.html")
+def list_books(request):
+   return render(request, 'bookmodule/list_books.html')
+def viewbook(request, bookId):
+   return render(request, 'bookmodule/one_book.html')
+def aboutus(request):
+   return render(request, 'bookmodule/aboutus.html')
 
 def index2(request, val1 = 0): 
    return render("value1 = "+str(val1))
